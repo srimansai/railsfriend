0c099bb6d61bd8c241d5a5afed8b49830f6976b5
sriman <sriman6907@gmail.com>
2022-11-05 18:55:29 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users
+ devise_scope :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+end
+  resources :friends
+ 
+  #get 'home/index'
+  #root 'home#index'
+  get 'home/about'
+  root 'friends#index'
+  #root 'friends#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
